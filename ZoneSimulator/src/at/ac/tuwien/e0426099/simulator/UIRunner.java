b80dfc7e01c3286301809c57f3b02fe72860effa
PatrickF <patrick.favrebulle@gmail.com>
2012-12-06 15:46:39 +0100

    intial

---
+++ ZoneSimulator/src/at/ac/tuwien/e0426099/simulator/UIRunner.java
@@ -0,0 +1,8 @@
+package at.ac.tuwien.e0426099.simulator;
+
+/**
+ * @author PatrickF
+ * @since 06.12.12
+ */
+public class UIRunner {
+}
---
+++ ZoneSimulator/src/at/ac/tuwien/e0426099/simulator/annotations/Service.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.e0426099.simulator.annotations;
+
+/**
+ * @author PatrickF
+ * @since 06.12.12
+ */
+public @interface Service {
+
+
+}
